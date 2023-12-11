a007a4fd6faddd06d6e414aab596eef5c9e7b6b7
BuhleMcetywa <buhlemcetywa07@gmail.com>
2023-12-11 14:39:42 +0200

    First commit

---
+++ NrstoSpokenWords/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace NrstoSpokenWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a four-digit number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number < 0 || number > 9999)
+            {
+                Console.WriteLine("Please enter a valid four-digit number.");
+
+            }
+
+            else if (number == 0)
+            {
+                Console.WriteLine("Zero");
+
+            }
+            else
+            {
+                Console.WriteLine(NumbersToWords.ConvertToWords(number));
+            }
+
+        }
+    }
+}
